ece7a6e1df5e8e9ccfa8299debbcccfc0999ee68
Farhan0511 <programingsukses.com>
2025-01-07 11:49:35 +0700

    add component desk and footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,54 @@
+<template>
+    <footer class="app-footer">
+        <div class="footer-content">
+            <p>&copy; 2025 My Company. All rights reserved.</p>
+            <nav class="footer-links">
+                <a href="/about">About Us</a>
+                <a href="/contact">Contact</a>
+                <a href="/privacy">Privacy Policy</a>
+            </nav>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "AppFooter",
+};
+</script>
+
+<style scoped>
+.app-footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 1rem 0;
+    bottom: 0;
+    width: 100%;
+    margin-top: 90px;
+    box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.2);
+}
+
+.footer-content {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.footer-links {
+    margin-top: 0.5rem;
+}
+
+.footer-links a {
+    color: #4caf50;
+    text-decoration: none;
+    margin: 0 0.5rem;
+    font-size: 0.9rem;
+}
+
+.footer-links a:hover {
+    text-decoration: underline;
+}
+</style>
---
+++ src/components/Deskripsi.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class=" text-dark text-center mt-5" >
+        <div>
+            <h2 class="fw-bold">Mengapa Harus Kami?</h2>
+        </div>
+
+        <div class="container py-5 p-2" style="background-color: #f8f9fa;">
+            <div class="row align-items-center">
+                <!-- Bagian Gambar -->
+                <div class="col-md-6 mb-4 mb-md-0">
+                    <img src="/batik3.png" alt="Cover Batik" class="img-fluid rounded shadow-lg w-100">
+                </div>
+                <!-- Bagian Teks -->
+                <div class="col-md-6">
+                    <h2 class="text-center text-md-start">
+                        Rumah Batik menyediakan batik tulis dan cap dengan desain yang memadukan tradisi dan modernitas.
+                        Kami juga menawarkan celana dan rok berbahan batik yang nyaman dan elegan. Setiap produk dibuat
+                        dengan perhatian terhadap detail dan kualitas, memberikan pengalaman istimewa mengenakan batik
+                        Indonesia.
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "DeskPage"
+}
+</script>
+
+<style></style>
