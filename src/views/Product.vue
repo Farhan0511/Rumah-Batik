69226997a435e2fe31272186162fde2746781770
Farhan0511 <programingsukses.com>
2025-01-13 09:50:38 +0700

    add views product

---
+++ src/views/Product.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div>
+        <navbar/>
+    </div>
+
+  <div class="text-center mt-5">
+    <Batik/>
+  </div>
+
+  <div>
+    <AppFooter/>
+  </div>
+  
+</template>
+
+<script>
+
+import Batik from '@/components/Batik.vue';
+import Navbar from '@/components/Navbar.vue';
+import AppFooter from '@/components/AppFooter.vue';
+
+export default {
+    name: "ProductView",
+    components: {
+     Batik,
+     Navbar,
+     AppFooter
+    }
+}
+</script>
+
+<style>
+
+</style>
