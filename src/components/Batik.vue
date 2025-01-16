f54f6bb27bcf8f31060a2a0c1c283aa71dff0ec3
Farhan0511 <programingsukses.com>
2025-01-16 22:24:07 +0700

    card content

--- src/components/Batik.vue
+++ src/components/Batik.vue
@@ -1,62 +1,24 @@
 <template>
-    <div>
-        <h2 class="fw-bold">Collection</h2>
-    </div>
 
-    <div class="container py-5">
-        <div class="row g-4 justify-content-center">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Card image">
-                    <div class="card-body">
-                        <h5 class="card-title">Card Title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Card image">
-                    <div class="card-body">
-                        <h5 class="card-title">Card Title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Card image">
-                    <div class="card-body">
-                        <h5 class="card-title">Card Title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="Card image">
-                    <div class="card-body">
-                        <h5 class="card-title">Card Title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
+    <div class="card" style="width: 18rem;">
+        <img :src="'/batik/' + product.gambar" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title fw-bold">{{ product.nama }}</h5>
+            <p class="card-text">{{ product.desk }}</p>
+            <router-link to="/product" class="btn btn-primary float-right">Explore
+                Now!</router-link>
         </div>
     </div>
+
 </template>
 
 <script>
+// import axios from 'axios';
 export default {
-    name: "BatikContent"
+    name: "BatikContent",
+    props: ['product']
 }
 </script>
 
-<style></style>
+<style>
+</style>
